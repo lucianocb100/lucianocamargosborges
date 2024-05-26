428d2c221a6134b966f2d3578d7a97656d4323fa
lcborges <luciano.camargos@scaladatacenters.com>
2024-05-25 23:51:51 -0300

    outrom commit

--- Sabor_Express/app.py
+++ Sabor_Express/app.py
@@ -12,7 +12,7 @@ def exibir_nome_do_programa():
 
 restaurantes = [{'nome':'Praça', 'categoria':'Japonesa', 'ativo':False}, 
                     {'nome':'Pizza Superma', 'categoria':'Pizza', 'ativo':True},
-                    {'nome':'Ciíjantina', 'categoria':'Italiano', 'ativo':False}]
+                    {'nome':'Ciíjantina', 'categorbgffbia':'Italiano', 'ativo':False}]
 
 def exibir_opcoes():
     print('1. Cadastrar restaurante')
